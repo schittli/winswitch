c97715d9876be746687bbb8d679185a66bf55ce0
konst.lvov <konst.lvov@f588a97a-ee99-11dd-901b-0b69d2339e2b>
2009-02-25 06:52:18 +0000

    Дважды был указан TaskSwitchXP.obj в списке модулей для линковки.

--- do_link.cmd
+++ do_link.cmd
@@ -3,4 +3,4 @@ rem LIB=E:\Program Files\Microsoft Visual Studio 8\VC\lib;E:\Program Files\Micro
 rem todo : create makefile
 if not exist Debug md Debug
 cd Debug
-link /VERBOSE:LIB /OUT:"TaskSwitchXP.exe" /INCREMENTAL /DEBUG /PDB:"TaskSwitchXP.pdb" /MAP /SUBSYSTEM:WINDOWS /DYNAMICBASE:NO /MACHINE:X86 /ERRORREPORT:PROMPT kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib TaskSwitchXP.obj generic.obj lang.obj main.obj TaskSwitchXP.obj tscontrol.obj TaskSwitchXP.res
+link /VERBOSE:LIB /OUT:"TaskSwitchXP.exe" /INCREMENTAL /DEBUG /PDB:"TaskSwitchXP.pdb" /MAP /SUBSYSTEM:WINDOWS /DYNAMICBASE:NO /MACHINE:X86 /ERRORREPORT:PROMPT kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib TaskSwitchXP.obj generic.obj lang.obj main.obj tscontrol.obj TaskSwitchXP.res
